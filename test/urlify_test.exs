f5d137855e2f77fc7468d5c7daade3d182a644f9
Andrés Gallego <andresgallego85@gmail.com>
2015-01-14 10:47:11 -0500

    change tabs to spaces

--- test/urlify_test.exs
+++ test/urlify_test.exs
@@ -1,17 +1,17 @@
 ExUnit.start
 
 defmodule UrlifyTest do
-	use ExUnit.Case, async: true
+  use ExUnit.Case, async: true
 
-	test "Get two post's url from twitter" do
-		assert Botixir.Urlify.convert("Hey look at this https://twitter.com/andresfortinero/status/555366730084929537
-			and https://twitter.com/julian_duque/status/555208342600425472 but not
-			https://twitter.com/andresfortinero") ==
-			["https://twitter.com/andresfortinero/status/555366730084929537",
-			 "https://twitter.com/julian_duque/status/555208342600425472"]
-	end
+  test "Get two post's url from twitter" do
+    assert Botixir.Urlify.convert("Hey look at this https://twitter.com/andresfortinero/status/555366730084929537
+      and https://twitter.com/julian_duque/status/555208342600425472 but not
+      https://twitter.com/andresfortinero") ==
+      ["https://twitter.com/andresfortinero/status/555366730084929537",
+       "https://twitter.com/julian_duque/status/555208342600425472"]
+  end
 
-	test "Given urls doesn't match with twitter posts" do
-		assert Botixir.Urlify.convert("https://twitter.com/julian_duque") == []
-	end
+  test "Given urls doesn't match with twitter posts" do
+    assert Botixir.Urlify.convert("https://twitter.com/julian_duque") == []
+  end
 end
