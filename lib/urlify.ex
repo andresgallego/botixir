6d6854f0ce20b832ea108450d9904dd8a2e1c839
Andrés Gallego <andresgallego85@gmail.com>
2015-01-21 18:56:38 -0500

    made more functional Urlify module

--- lib/urlify.ex
+++ lib/urlify.ex
@@ -1,7 +1,11 @@
 defmodule Botixir.Urlify do
-  def convert(text) do
+  def convert text do
     urlRegex = ~r{http(?:s)?\:\/\/(?:www.)?twitter.com\/[0-9a-zA-Z_-]*\/[0-9a-zA-Z_-]*\/([0-9])*}i
     match = Regex.scan(urlRegex, text, capture: :first)
-    match |> List.flatten |> Botixir.Twstream.getUrls
+    match |> List.flatten
+  end
+
+  def call_twitter urls do
+    urls |> Botixir.Twstream.getUrls
   end
 end
