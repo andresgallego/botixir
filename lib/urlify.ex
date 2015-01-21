d58f5d2b0c460f8e66a701f4d6b0e800d523f813
Andrés Gallego <andresgallego85@gmail.com>
2015-01-20 20:05:09 -0500

    change delimiters in regex

--- lib/urlify.ex
+++ lib/urlify.ex
@@ -1,6 +1,6 @@
 defmodule Botixir.Urlify do
   def convert(text) do
-    urlRegex = ~r/http(?:s)?\:\/\/(?:www.)?twitter.com\/[0-9a-zA-Z_-]*\/[0-9a-zA-Z_-]*\/([0-9])*/
+    urlRegex = ~r{http(?:s)?\:\/\/(?:www.)?twitter.com\/[0-9a-zA-Z_-]*\/[0-9a-zA-Z_-]*\/([0-9])*}i
     match = Regex.scan(urlRegex, text, capture: :first)
     match |> List.flatten |> Botixir.Twstream.getUrls
   end
